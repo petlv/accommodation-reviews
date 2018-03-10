415aa97d3adf407fd40fd718f6ef26c27f9c8e41
Petar Ivanov <petlv@mail.bg>
2018-03-10 20:10:10 +0200

    Initial configuring

---
+++ src/main/org.softuni.accommodationreviews/AccommodationReviewsApplication.java
@@ -0,0 +1,12 @@
+package org.softuni.accommodationreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationReviewsApplication {
+    public static void main(String[] arg) {
+        SpringApplication.run(AccommodationReviewsApplication.class, arg);
+    }
+
+}

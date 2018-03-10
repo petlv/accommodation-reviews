819a0b33cd17ee3898e327cf15000453ad1d0f44
Petar Ivanov <petlv@mail.bg>
2018-03-11 01:47:34 +0200

    Bootstrap configuring

---
+++ src/main/org.softuni.accommodationreviews/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package org.softuni.accommodationreviews.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public ModelAndView index (ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        modelAndView.addObject("message", "Pete");
+        return modelAndView;
+    }
+
+}
